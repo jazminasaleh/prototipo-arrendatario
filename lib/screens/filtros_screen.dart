195789040b6ae33de6f336b214e9954b23dce1c8
jazmin saleh peña <jazminasaleh@gmail.com>
2022-08-13 17:58:03 -0500

    Resultados de filtros

--- lib/screens/filtros_screen.dart
+++ lib/screens/filtros_screen.dart
@@ -17,14 +17,14 @@ class FiltrosScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text(
-                    '',
+                    'Reusltados de la busqueda',
                     style: TextStyle(
                         color: Colors.black,
                         fontSize: 20,
                         fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
-                    width: 80,
+                    width: 20,
                   ),
                   Icon(
                     Icons.person,
@@ -41,9 +41,9 @@ class FiltrosScreen extends StatelessWidget {
           children: [
             Container(
                decoration: BoxDecoration(
-                  color: Colors.green, borderRadius: BorderRadius.circular(20)),
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
                 child: SizedBox(
-                  height: 300,
+                  height: 400,
                   width: 390,
                   child: Column(
                     children: [
@@ -56,23 +56,51 @@ class FiltrosScreen extends StatelessWidget {
         SizedBox(height: 10,),
         Row(
           children: [
-            Icon(
-              Icons.attach_money,
-            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.attach_money)),
             Text('600,000')
           ],
         ),
-        SizedBox(
-          height: 20,
-        ),
         Row(
           children: [
-            
+            IconButton(onPressed: (){}, icon: Icon(Icons.location_on_outlined)),
+            Text('Cra 10 # 10 - 98')
           ],
-        )
+        ),
+        SizedBox(height: 15,),
+        Row(
+          children: [
+             Text('     Caracteristicas', style: TextStyle(color: Colors.grey),),
+          ],
+        ),
+        SizedBox(height: 8,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Icon(Icons.bed_sharp),
+           SizedBox(width: 2,),
+           Text('4'),
+           SizedBox(width: 27,),
+           Icon(Icons.bathtub_sharp),
+           SizedBox(width: 2,),
+           Text('3'),
+            SizedBox(width: 27,),
+           Icon(Icons.garage),
+           SizedBox(width: 2,),
+           Text('1'),
+            SizedBox(width: 27,),
+           Icon(Icons.stacked_bar_chart_outlined),
+           SizedBox(width: 2,),
+           Text('5'),
+            SizedBox(width: 27,),
+           Icon(Icons.wifi),
+           SizedBox(width: 2,),
+           Text('10'),
+           
 
+          ],
+        ),
         ],
-                  )),
+              )),
             )
           ],
         ),
